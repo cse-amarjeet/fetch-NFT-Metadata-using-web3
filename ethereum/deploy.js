1a487fbd6a5e22c97eaaf2602ae4f15c12e7aa43
Amarjeet Kumar <ajroni98@gmail.com>
2023-02-07 12:27:15 +0530

    Update deploy.js
    remove Phase of meta mask

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -2,9 +2,9 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 
 const provider = new HDWalletProvider(
-  "tortoise uphold assume theme daring unfold cheese erode margin picnic heavy coach",
+  "phase of meta mask",
   // remember to change this to your own phrase!
-  "https://goerli.infura.io/v3/6d91b4f97959471fb5046ce94d105475"
+  "infura ethereum node provider"
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
